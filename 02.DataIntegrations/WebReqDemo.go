ec33b50fb7924c9e764d8182fa54b47d619381d3
mujahed85 <mujahed.mca@gmail.com>
2023-01-13 12:00:47 +0530

    Restructure and WebReqDemo

---
+++ 02.DataIntegrations/WebReqDemo.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Web with GO")
+	fmt.Println("Data Transfering to Console ")
+}
+
+func main() {
+	http.HandleFunc("/", indexPage)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
