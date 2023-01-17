bb634b19f0cb8560319531fa15a33d2e40757976
Ash-exp <asutosh2000ad@gmail.com>
2023-01-17 14:25:53 +0530

    SQL using go

---
+++ 00.Activity/AsutoshDas/Day5-Assignments/SQL Using Go/SimpleDB.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	db, err := sql.Open("mysql", "root:12345@tcp(127.0.0.1:3306)/a1")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	// INSERT INTO T1(Id,Name) VALUES(3,'CCC')
+	fmt.Println("Inserting Data")
+	insert, err := db.Query("INSERT INTO T1(Id,Name) VALUES(4,'CCC')")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer insert.Close()
+
+}
