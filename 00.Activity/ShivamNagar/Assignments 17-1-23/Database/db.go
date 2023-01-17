f7db90a567c7881ce62cc8fd86204aeca5de1b65
Shivam nagar <nagarshivam409@gmail.com>
2023-01-17 12:27:03 +0530

    sql Db

---
+++ 00.Activity/ShivamNagar/Assignments 17-1-23/Database/db.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+func main() {
+
+	db, err := sql.Open("mysql", "root:123@tcp(127.0.0.1:3306)/a1")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	// INSERT INTO T1(Id,Name) VALUES(3,'CCC')
+	fmt.Println("Inserting Data")
+	insert, err := db.Query("INSERT INTO T1(Id,Name) VALUES(3,'CCC')")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer insert.Close()
+
+}
